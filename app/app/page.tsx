eeecc22bfefc7a1dd4128b09ecc7d26ec39a9368
Christopher Lyon <31580672+ChristopherLyon@users.noreply.github.com>
2024-05-29 08:54:10 +0200

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -28,6 +28,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea"
+import { toast } from "sonner"
 
 export default function App() {
   const [notes, setNotes] = useState<INote[]>([]);
@@ -52,6 +53,67 @@ export default function App() {
     return () => clearInterval(interval);
   }, []);
 
+
+  // State for raw_input
+  const [rawInput, setRawInput] = React.useState("");
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+
+  const minimumNoteLength = 20;
+  // Progress is grom 0-100, calculate the percentage of the minimum note length
+  const progress = (rawInput.length / minimumNoteLength) * 100;
+
+  // Submit function
+  const handleSubmit = async () => {
+    // If rawInput is empty, is "" or has only spaces, show a sonner toast and return
+    if (rawInput === "" || rawInput.trim() === "") {
+      toast.error("Note cannot be empty");
+      return;
+    }
+
+    // If the note is less than the minimum length, show a sonner toast and return
+    if (rawInput.length < minimumNoteLength) {
+      toast.error(`Note must be at least ${minimumNoteLength} characters long`);
+      return;
+    }
+
+    try {
+      // Show success toast and reset the form
+      toast.info("Stream added to AI queue 🚀");
+      setRawInput("");
+      setDialogOpen(false);
+
+      const response = await fetch("/api/backend/notes", {
+        method: "POST",
+        body: JSON.stringify({ raw_note: rawInput, source: "web" }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      toast.success("Stream added successfully");
+
+      if (!response.ok) {
+        throw new Error("Failed to add note");
+      }
+
+    } catch (error) {
+      toast.error("Failed to add note");
+    }
+  };
+
+  // If enter is pressed at any time, as long as shift is not pressed, submit the note and close the dialog
+  React.useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        handleSubmit();
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [rawInput]);
+
+
   return (
     <main className="h-full flex flex-row gap-4">
       {fetchingNotes ? (
@@ -66,7 +128,7 @@ export default function App() {
         </div>
       )}
       <div className="w-1/4 flex flex-col gap-4">
-        <Textarea className="w-full h-[340px] bg-background border-dashed" placeholder="Quick Report" />
+        <Textarea className="w-full h-[340px] bg-background border-dashed" placeholder="Quick Report" value={rawInput} onChange={(e) => setRawInput(e.target.value)} />
         <Card className="w-full h-full flex flex-col bg-background border-dashed">
         </Card>
       </div>
